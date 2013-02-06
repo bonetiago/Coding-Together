00de65e4d18747cedbc8298df48b6b33a4bb84a3
Tiago Couto <tiago.couto@me.com>
2013-02-06 07:34:01 -0200

    + Correções + CardMatchingGame.m/.h

---
+++ Matchismo/Matchismo/Model/CardMathchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMathchingGame.h
+//  Matchismo
+//
+//  Created by Tiago Couto on 06/02/13.
+//  Copyright (c) 2013 dryBone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMathchingGame : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Model/CardMathchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMathchingGame.m
+//  Matchismo
+//
+//  Created by Tiago Couto on 06/02/13.
+//  Copyright (c) 2013 dryBone. All rights reserved.
+//
+
+#import "CardMathchingGame.h"
+
+@implementation CardMathchingGame
+
+@end
